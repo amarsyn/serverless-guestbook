d4c67ea95c3296588d66718f66dfcd9146a67265
amarsyn <33958969+amarsyn@users.noreply.github.com>
2022-07-21 23:44:46 +0530

    Update guestbook.js
    API URL added

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://98371996.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
